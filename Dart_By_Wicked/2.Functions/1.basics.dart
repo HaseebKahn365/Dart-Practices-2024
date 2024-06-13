65dd39593ded4a0d4111c8a58d9bec1d52ef1771
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-06-13 19:19:50 +0500

    learnt functions advanced

---
+++ Dart_By_Wicked/2.Functions/1.basics.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  var first = neon;
+  var second = customPrinter;
+  print(exectuteFirst(first, 23, 123));
+
+  printThis(second, 'My name is haseeb');
+
+  var names = ['haseeb', 'bilal'].map((e) => e.toUpperCase()).toList();
+  print(names);
+}
+
+void printThis(void second(String s), String passed) {
+  second(passed);
+}
+
+void customPrinter(String s) {
+  print("😰👍✅⭐😄😘🥺\n\n$s wow\n\n😄😘😘");
+}
+
+int exectuteFirst(int Function(int, int) first, int firstInt, int secondInt) {
+  return first(firstInt, secondInt);
+}
+
+int neon(int a, int b) {
+  return a + b;
+}
